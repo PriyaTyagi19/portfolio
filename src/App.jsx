4c4323317dd3073f85be0dde870c29b4802ea8d1
Priya Tyagi <priyatyagi1905@gmail.com>
2024-07-09 21:14:45 -0700

    added social icons and navigation icons also added styles for the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,88 @@
-import { Route, Routes, Link, NavLink } from 'react-router-dom'
-import Home from './templates/Home'
-import About from './templates/About'
-import Posts from './templates/Posts'
-import Post from './templates/Post'
-import Connect from './templates/Connect'
-import './styles/style.css'
-import logo from '../src/assets/logo.svg'
-
+import { Route, Routes, Link, NavLink } from "react-router-dom";
+import Home from "./templates/Home";
+import About from "./templates/About";
+import Posts from "./templates/Posts";
+import Post from "./templates/Post";
+import Connect from "./templates/Connect";
+import homeicon from "../src/assets/homeicon.svg";
+import workicon from "../src/assets/workicon.svg";
+import connecticon from "../src/assets/connecticon.svg";
+import abouticon from "../src/assets/abouticon.svg";
+import linkedinicon from "../src/assets/linkedinicon.svg";
+import githubicon from "../src/assets/github.svg";
+import logo from "../src/assets/logo.svg";
 
 function App() {
-
   return (
     <>
       <header id="masthead" className="site-header">
-      <img src={logo} alt="A logo" className="logo" id="logo" />
+        <a href="/">
+          <img src={logo} alt="A logo" className="logo" id="logo" />
+        </a>
         <nav className="site-navigation">
           <ul>
-            <li><NavLink to='/' end>Home</NavLink></li>
-            <li><NavLink to='/about'>About</NavLink></li>
-            <li><NavLink to='/work'>Works</NavLink></li>
-            <li><NavLink to='/connect'>Connect</NavLink></li>
+            <li>
+              {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill={remove === false ? "#3ff0af" : "#ff0000"} d="M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z"/></svg> */}
+              <NavLink to="/" end>
+                <img src={homeicon} alt="a home icon"></img>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">
+                <img src={abouticon} alt="an about icon"></img>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/work">
+                <img src={workicon} alt="a work icon"></img>
+                Works
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/connect">
+                <img src={connecticon} alt="a connect icon"></img>
+                Connect
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </header>
       <main id="main">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/work' element={<Posts />} />
-          <Route path='/work/:slug' element={<Post />} />
-          <Route path='/connect' element={<Connect />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work" element={<Posts />} />
+          <Route path="/work/:slug" element={<Post />} />
+          <Route path="/connect" element={<Connect />} />
         </Routes>
       </main>
       <footer>
-        <nav className="site-navigation">
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><NavLink to='/work'>Works</NavLink></li>
-            <li><Link to='/connect'>Connect</Link></li>
-          </ul>
-        </nav>
-       <p className="copyright"> 2024 Designed and Developed by Priya Tyagi</p>
+        <div className="social-icons">
+
+        <a
+          href="https://www.linkedin.com/in/priya-tyagi-600737249/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedinicon} alt="A linkedin Icon" />
+        </a>
+
+        <a
+          href="https://github.com/PriyaTyagi19"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={githubicon} alt="A github Icon" />
+        </a>
+        </div>
+        <p className="copyright">
+          &copy; <span> 2024 Designed and Developed by Priya Tyagi</span>
+        </p>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/templates/Connect.jsx
+++ src/templates/Connect.jsx
@@ -1,42 +1,64 @@
-import { useState, useEffect } from 'react'
-import Loading from '../utilities/Loading'
-import { restBase } from '../utilities/Utilities'
+import { useState, useEffect } from "react";
+import Loading from "../utilities/Loading";
+import { restBase } from "../utilities/Utilities";
+import linkedinicon from "../assets/linkedinicon.svg";
+import githubicon from '../assets/github.svg';
 
-const  Connect= () => {
-    const restPath = restBase + 'pages/14'
-    const [restData, setData] = useState([])
-    const [isLoaded, setLoadStatus] = useState(false)
+const Connect = () => {
+  const restPath = restBase + "pages/14";
+  const [restData, setData] = useState([]);
+  const [isLoaded, setLoadStatus] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(restPath)
-            if ( response.ok ) {
-                const data = await response.json()
-                setData(data)
-                setLoadStatus(true)
-            } else {
-                setLoadStatus(false)
-            }
-        }
-        fetchData()
-    }, [restPath])
-    
-    return (
-        <>
-        { isLoaded ?
-            <article id={`post-${restData.id}`}>
-                
-                <div className="entry-content" dangerouslySetInnerHTML={{__html:restData.content.rendered}}>
-                </div>
-                    <a href="mailto:priyatyagi1905@gmail.com">Email Me!</a>
-                    <p>Reach out to me through LinkedIn </p>
-                    
-            </article>
-        : 
-            <Loading /> 
-        }
-        </>            
-    )
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(restPath);
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
+    };
+    fetchData();
+  }, [restPath]);
 
-export default Connect
+  return (
+    <>
+      {isLoaded ? (
+        <article id={`post-${restData.id}`}>
+          <div
+            className="entry-content"
+            dangerouslySetInnerHTML={{ __html: restData.content.rendered }}
+          ></div>
+          <div className="email-section">
+          <a href="mailto:priyatyagi1905@gmail.com">Email Me!</a>
+          <p>Reach out to me through LinkedIn </p>
+          </div>
+          
+          <div className="social-icons">
+            <a
+              href="https://www.linkedin.com/in/priya-tyagi-600737249/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedinicon} alt="A linkedin Icon" />
+            </a>
+
+            <a
+              href="https://github.com/PriyaTyagi19"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={githubicon} alt="A github Icon" />
+            </a>
+          </div>
+        </article>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
+};
+
+export default Connect;
